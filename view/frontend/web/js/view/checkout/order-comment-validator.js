927009698f19477ac2de03cc8bd15e0b443d6cfa
steven <steven@livz.org>
2018-09-18 00:26:47 +0100

    s

--- view/frontend/web/js/view/checkout/order-comment-validator.js
+++ view/frontend/web/js/view/checkout/order-comment-validator.js
@@ -12,21 +12,3 @@ define(
         return Component.extend({});
     }
 );
-
-
-//require(["jquery"], function($) {
-//    jQuery(document).ready(function(){
- //       console.log('xxxxxxxxxxxxxxxxxxxxx');
-        //jQuery('body').on('click', '.table-checkout-shipping-method input[type="radio"]', function(){
-           // var code = 'your_custom_shipping_method_code';
-            // you can check your custom shipping method code using inspect element
-            // you can see the code in the value of input type radio
-
-            //if(jQuery(this).val() == code){
-            //    jQuery('.custom-shipping-method-message').show();
-            //} else {
-            //    jQuery('.custom-shipping-method-message').hide();
-            //}
-        //});
- //   });
-//});
