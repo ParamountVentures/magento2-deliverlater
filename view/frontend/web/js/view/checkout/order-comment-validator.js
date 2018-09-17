777550685ade214c4df39fbd85a01af3ae94b6fa
steven <steven@livz.org>
2018-09-17 23:42:38 +0100

    s

--- view/frontend/web/js/view/checkout/order-comment-validator.js
+++ view/frontend/web/js/view/checkout/order-comment-validator.js
@@ -13,11 +13,31 @@ define(
     }
 );
 
-$( document ).ready(function() {
-    console.log( "ready!" );
 
-    $( "#banktransfer" ).click(function() {
-        alert( "Handler for banktransfer.click() called." );
-      });
+require([
+    'jquery',
+    'Magento_Checkout/js/model/quote',
+    'Magento_Checkout/js/model/shipping-service',
+    'Magento_Checkout/js/model/shipping-rate-registry',
+    'Magento_Checkout/js/model/shipping-rate-processor/customer-address',
+    'Magento_Checkout/js/model/shipping-rate-processor/new-address',
+], function($, quote, shippingService, rateRegistry, customerAddressProcessor, newAddressProcessor) {
+    $('#banktransfer').on('click', function(e) {
+        var address = quote.shippingAddress();
+        console.log(address);
+
+        // reload address information
+        address.trigger_reload = new Date().getTime();
+
+        // clearing cached rates to retrieve new ones
+        rateRegistry.set(address.getCacheKey(), null);
+
+        console.log(quote.shippingAddress()); // but getting old one 
+        var type = quote.shippingAddress().getType();
+        if (type) {
+            customerAddressProcessor.getRates(address);
+        } else {
+            newAddressProcessor.getRates(address);
+        }
+    });
 });
-
